9752d0a1b610099a6625e16b1dd8c750ace313e4
gelinas <jlgelinas@gmail.com>
2020-02-01 22:31:30 -0500

    configure deployment postgres database

--- knexfile.js
+++ knexfile.js
@@ -64,24 +64,13 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true, // needed for sqlite
-    connection: {
-      filename: './data/data.db3'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
     },
-    // needed when using foreign keys
-    pool: {
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-      },
-    },
   }
-
 };
